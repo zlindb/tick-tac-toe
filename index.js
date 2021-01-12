24feba72367eb488d7b0bdf1c4725a48bad388c2
user <zlindb210@gmail.com>
2021-01-12 18:05:41 -0500

    working, no ai

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
 //factory function
-const Player = (name, marker) =>{
+const Player = (name, marker, type) =>{
 	const getName = () => name;		//getter
 	const getMarker = () => marker;	//getter
-	//shorthand
-	const turn = false;
+	const getType = () => type;
+
+	const move = (board, pos) =>{
 	
-	return { getName, getMarker, turn };
+		if(getType() === 'ai'){
+			let ran = Math.floor(Math.random() * Math.floor(9)); 	//return # 1-8
+			board.setBoard(ran, getMarker());	
+
+		}
+		else{
+			
+		//	gameboard.setBoard(pos, getMarker());
+		}
+	}
+	
+	return { getName, getMarker, getType, move};
 }
 
 const Gameboard = () =>{
-	const gameboard = [];
+	const gameboard = [
+		'','','',
+		'','','',
+		'','',''
+	];
 
 	const getBoard = () => gameboard;
-	const setBoard = (pos, val) =>{
-		if(typeof(val) !== "string"){
+	const setBoard = (pos, marker) =>{
+		if(typeof(marker) !== "string"){
 			console.error("not a string");
 			return;
 		}
 		//check if value is either X or O
-		if(val.toUpperCase() === 'X' || val.toUpperCase() === 'O'){
-			gameboard[pos] = val.toUpperCase();		
+		if(marker.toUpperCase() === 'X' || marker.toUpperCase() === 'O'){
+			gameboard[pos] = marker.toUpperCase();		
 		}			
 	}
 
 	const clearBoard = () =>{
 		gameboard.length = 0;
-		return;
 	}
 
 	return { getBoard, setBoard, clearBoard };
 }
 
 //object literal 
-const DisplayController = (selector = '#gameboard') => {
+const DisplayController = ((selector = '#gameboard') => {
 
 	return {
 		board: document.querySelector(selector),
-		createElement: (element, attr) => {
+		createElement: (element, attr=['class', '']) => {
 			let ele = document.createElement(element)
 			ele.setAttribute(attr[0], attr[1]);
 			return ele;
@@ -52,165 +67,242 @@ const DisplayController = (selector = '#gameboard') => {
 				}
 				this.board.appendChild(cellrow) //append to gameboard
 			}
+		},
+		writeToDom: (selector, msg) =>{
+			selector.innerHTML = msg;
 		}
-		
-
 	}
-}
+})('#gameboard')
+//})('#gameboard').buildGrid(); //self invoked to build grids
+
+//build dom gameboard
+DisplayController.buildGrid(); 
+
 
 //Module pattern function 
 const GameControl = (()=>{
-//	let turn = true;
-	let p_loop = 0;
+
+	const gamecells = document.querySelectorAll('#gameboard .cell');
 	
-	const writeToDom = function(selector, msg){
-		selector.innerHTML = msg;
-	}
-	//game start
-	const init = ()=>{
-		
-		//build dom gameboard
-		DisplayController('#gameboard').buildGrid(); 
+	let p1name = document.getElementById('player1name').value || "player1";
+	let p2name = document.getElementById('player2name').value || "player2";
 
-		//create the board
-		let gameboard = Gameboard();
-		console.log("init board");
+	DisplayController.writeToDom(document.getElementById('p1name'), p1name);
+	DisplayController.writeToDom(document.getElementById('p2name'), p2name);
 
-		//creating the players
-		let player1 = Player('player1', 'X');
-		let player2 = Player('player2', 'O');
-	//	let player3 = Player('player3', 'Z');
-	//	let player4 = Player('player4', 'W');
+	//board, players declaration
+	console.log("init board");
+	let	gameboard = Gameboard();
 
-		console.log("created two players");
-
-		//specific which player to take first turn
-		player1.turn = true;
-
-		//start the game by passing in players, and board
-		gamestart(gameboard, [player1, player2]);
-	
-	}
-	
-	//Multiple player turn control, can work more than 2
-	//@param players - Array Objects of all player created
-	//@param firstturn - player object who's taking the first turn
-	const turnControl = (players)=>{
-
-		let currentPlayer = '';
-
-		if(players[p_loop].turn === true){
-			
-			currentPlayer = players[p_loop];
-			
-			//turn off current player turn
-			currentPlayer.turn = !currentPlayer.turn; 
-
-			//next turn
-			p_loop++;
-
-			//if next player does not exist (out of bound)
-			if(!players[p_loop]){
-				//set next player to player 1
-				p_loop = 0; 
-				players[p_loop].turn = !players[p_loop].turn;		
-			}
-			else{			
-				//set next player turn to true
-				players[p_loop].turn = !players[p_loop].turn;
-			}
-		}
-		console.log("current player: ", currentPlayer);
-		return currentPlayer;		
-	}
-
-
-	const gamestart = (gameboard, players, callback) =>{
-		//let { player1, player2, gameboard } = init();
-		let cells = document.querySelectorAll('#gameboard .cell');	
-		
-		for(let i = 0; i< cells.length; i++){
-			cells[i].addEventListener('click', function flow(e) {
-			
-				//cell already clicked, do nothing
-				if(this.innerText !== ''){
-					//this.removeEventListener(e.type, flow, false ); //remove listener
-					return;
-				}
-			
-				//if(turn == true) {	
-
-				let player_turn = turnControl(players);
-				console.log('who: ' + player_turn.getName());
-
-
-				//update the dom
-				//this.innerText = player1.getMarker();
-				//e.target.innerText = player1.getMarker();
-				writeToDom(e.target, player_turn.getMarker());
-
-				//update the board
-				//console.log(Array.prototype.indexOf.call(cells, this))
-
-				//use call method to use indexOf from array to get cells
-				gameboard.setBoard(Array.prototype.indexOf.call(cells, this), player_turn.getMarker());
-
-				if(gameover(gameboard.getBoard()) === true){
-					//pass in callback function to update the dom
-					//and clear up the board
-					console.log('winner p1');
-				}	
-		
-				console.log('onclick: cell# '+i+ " cellval " + gameboard.getBoard()[i]);	
-			}, false);
-
-		}
-		//move();
-		//get turn
-	}
-
-	const gameover = (gameboard) =>{
-		//no checks when there's less than 3 marks
-		let winner = false;
-
-		//value are 1 less than actual number pattern
-		//winning scenario
-		let wp = [ 
-			'012', '036', '048',
-			'147', '258', '246',
-			'345', '678'
+	console.log("created two players");		
+	let playerArr =[
+			Player(p1name, 'X', 'ai'),
+			Player(p2name, 'O', 'human')
 		];
 
-		//if there's less 3 marks, skip checks
-		//filter array to ignore empty values
-		if(gameboard.filter(function(val){
-			if(!val) return false;
-			return val;
-		}).length < 3) return;
-		//winning_pattern.forEach(function(e){
+	let p_loop = 0;
+/*
+	const updateStatus = (msg) =>{
+		let container = document.querySelector('#status');
+		container.prepend(document.createTextNode(msg))
+	}
+*/
+	//game start
+	const init = ()=>{	
+
+		//check if player is AI
+		if(playerArr[p_loop].getType() === 'ai'){
+			aiMove();
+			nextTurn();	
+		}
+		//attach event listener
+		for(let i = 0; i< gamecells.length; i++){
+			gamecells[i].addEventListener('click', eventListener);
+		}
+
+	}
+	const nextTurn = () =>{
+		p_loop = (p_loop+1) % playerArr.length;
+
+		if(playerArr[p_loop].getType() === 'ai'){	//ai turn;
+			aiMove();
+
+			if(checkWinner() === 'true'){
+				disable_cell();
+				console.log('Winner: ' + playerArr[p_loop].getName());
+				return;
+			}
+			else if(checkWinner() === 'tied'){
+				disable_cell();
+				console.log('tied');
+				return;
+			}
+		
+			else{
+				nextTurn();
+			}
+		}		
+	}
+
+	//get the available spot in gamboard
+	const availableIndex = () =>{
+		let available = [];	
+		gameboard.getBoard().forEach((val,index)=>{
+			if(val === '') available.push(index);
+		});
+		let random = Math.floor(Math.random() * available.length);	
+		return available[random];
+	}
+
+	const aiMove = ()=>{
+		let index = availableIndex();
+		let bestMove;
+		let bestscore = -1000000;
+
+		//if(gameboard.getBoard(index) === ""){
+		/*	
+			let score = minimax(gameboard.getBoard(), 0, true);
+			console.log(score);
+			if(score > bestscore){
+				bestscore = score;
+				bestMove = index;
+			}
+*/
+		//	console.log(bestMove);
+
+			gameboard.setBoard(index, playerArr[p_loop].getMarker());	
+			DisplayController.writeToDom(gamecells[index], playerArr[p_loop].getMarker());
+
+		//	gameboard.setBoard(bestMove, playerArr[p_loop].getMarker());	
+		//	DisplayController.writeToDom(gamecells[bestMove], playerArr[p_loop].getMarker());
+		//}
+
+	}
+
+	const eval_func = {
+		'true': 10,
+		'false': -10,
+		'tied': 0
+	}
+	//const minimax = (position, depth, isMaxPlayer) =>{
+	const minimax = (board, depth, isMaxPlayer) =>{
+		let result = checkWinner();
+		if(result !== 'false'){	
+			console.log("result", result);
+			return eval_func[result];
+		}
+		console.log(board);
+		if(isMaxPlayer){
+			let maxScore = -Infinity;
+			for(let i=0; i< board.length; i++){
+				if(board[i] === ""){
+
+					board[i] = playerArr[0].getMarker();
+					let score = minimax(board, depth+1, false);
+					//what does the score return?
+					//what is the value function?
+
+					board.setBoard(i, '');
+
+					maxScore = Math.max(maxScore, score);
+					
+				}
+				
+			}
+			return maxScore;		
+		}
+		else{ //min player
+			let maxScore = Infinity;
+			for(let i=0; i< board.length; i++){
+				if(board[i] === ""){
+
+					board[i] = playerArr[1].getMarker();
+					let score = minimax(board, depth+1, true);
+					board.setBoard(i, '');
+
+					maxScore = Math.min(score, maxScore);
+					
+				}
+				
+			}
+			
+			return maxScore;
+		}
+
+		return 1;
+	}
+
+	//event Listener for human player
+	const eventListener = (e) =>{	
+		if(event.target.innerText === ''){
+
+			gameboard.setBoard(Array.prototype.indexOf.call(gamecells, e.target), playerArr[p_loop].getMarker());
+			DisplayController.writeToDom(e.target, playerArr[p_loop].getMarker());
+		
+			if(checkWinner() === 'true'){
+				disable_cell();
+				console.log("winner ", playerArr[p_loop].getName());
+
+			}
+			else if(checkWinner() === 'tied'){
+				disable_cell();
+				console.log('game tied');
+			}
+			else{
+				nextTurn();
+			}
+			
+		}
+	}
+
+	const disable_cell = () => {
+		//remove all event listener to stop progress
+		/*gamecells.forEach(node=>{
+			node.removeEventListener(type, func, false);
+		});*/
+
+		gamecells.forEach(node =>{
+			node.classList.add('disable-div');
+		});
+	}
+
+	const checkWinner = () =>{
+	
+		let winner = 'false';
+	
+		//winning scenario
+		let wp = [ '012', '036', '048',	'147', '258', '246','345', '678'];
+
+		//if there's less 3 marks, skip checks //filter array to ignore empty values
+		// if(gameboard.getBoard().filter(function(val){
+		// 	if(!val) return false;
+		// 	return val;
+		// }).length < 3) return false;
+		
 		for(let i = 0; i< wp.length; i++){
-			//gameboard.getBoard(); //get all the board value
-			//check for winning condition
-			//e.toString()[0]
 			let e = wp[i];
-			let win = gameboard[e[0]] + gameboard[e[1]] +gameboard[e[2]];
+
+			let win = gameboard.getBoard()[e[0]] + gameboard.getBoard()[e[1]] +gameboard.getBoard()[e[2]];
 
 			if(win === 'XXX' || win === 'OOO'){
-				winner = true;
+				winner = 'true';	
+			//	console.log("winner is: ", playerArr[p_loop].getName());
+
 				return winner;
 			}
+			
 		}
-		
 		//no winner decided, return tied game
-		if(gameboard.filter(function(val){
+		if(gameboard.getBoard().filter(function(val){
 			if(!val) return false;
 			return val;
-		}).length === 9 && winner == false){
-			console.log('game tied')
+		}).length === 9 && winner == 'false'){
+			return 'tied';
 		}
-		
-		return winner;
+		return 'false';
 	}
-	return { gamestart, gameover, init }
+
+	return { init }
 })();
+
 
